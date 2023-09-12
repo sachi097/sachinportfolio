52f470812742f61fd6cbb74102e03d6181e27d4e
sachi097 <nieesachi.097@gmail.com>
2023-09-12 02:53:30 -0600

    Updates

---
+++ precache-manifest.a8a693cb7f46a0ad82947d1774ba1c4e.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e14651fab7525ddda699e506cb9cba",
+    "url": "/sachinportfolio/index.html/index.html"
+  },
+  {
+    "revision": "8d558a198f039e7b5e1e",
+    "url": "/sachinportfolio/index.html/static/css/main.0fe7901f.chunk.css"
+  },
+  {
+    "revision": "13153c0f4976037f220c",
+    "url": "/sachinportfolio/index.html/static/js/2.5c6d82ef.chunk.js"
+  },
+  {
+    "revision": "8d558a198f039e7b5e1e",
+    "url": "/sachinportfolio/index.html/static/js/main.27e73155.chunk.js"
+  },
+  {
+    "revision": "53a911a3adf3e5fc4cb8",
+    "url": "/sachinportfolio/index.html/static/js/runtime-main.c02e5e58.js"
+  },
+  {
+    "revision": "2fca1bfdfbfb17d260a8faf7bbda5537",
+    "url": "/sachinportfolio/index.html/static/media/20200630_162659.2fca1bfd.gif"
+  },
+  {
+    "revision": "19c598ff541ae08acfc5139098f34a6a",
+    "url": "/sachinportfolio/index.html/static/media/AI_ML.19c598ff.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/sachinportfolio/index.html/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/sachinportfolio/index.html/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/sachinportfolio/index.html/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/sachinportfolio/index.html/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/sachinportfolio/index.html/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/sachinportfolio/index.html/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/sachinportfolio/index.html/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "d688d5e1ea4f191c61644dba755238a5",
+    "url": "/sachinportfolio/index.html/static/media/Home.d688d5e1.gif"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/sachinportfolio/index.html/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/sachinportfolio/index.html/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "eaf02c2d0158b6b4b8c2a0a0cf830a04",
+    "url": "/sachinportfolio/index.html/static/media/animated_ashutosh.eaf02c2d.png"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/sachinportfolio/index.html/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/sachinportfolio/index.html/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "cacbc42b661f39344af81c631d8d36a9",
+    "url": "/sachinportfolio/index.html/static/media/cloud_infrastructure.cacbc42b.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/sachinportfolio/index.html/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/sachinportfolio/index.html/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "9f4a550b766aac1ae46f7c49e75081f7",
+    "url": "/sachinportfolio/index.html/static/media/cub_logo.9f4a550b.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/sachinportfolio/index.html/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/sachinportfolio/index.html/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/sachinportfolio/index.html/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/sachinportfolio/index.html/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/sachinportfolio/index.html/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/sachinportfolio/index.html/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/sachinportfolio/index.html/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/sachinportfolio/index.html/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/sachinportfolio/index.html/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/sachinportfolio/index.html/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/sachinportfolio/index.html/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/sachinportfolio/index.html/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/sachinportfolio/index.html/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/sachinportfolio/index.html/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/sachinportfolio/index.html/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/sachinportfolio/index.html/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/sachinportfolio/index.html/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/sachinportfolio/index.html/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/sachinportfolio/index.html/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/sachinportfolio/index.html/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/sachinportfolio/index.html/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/sachinportfolio/index.html/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/sachinportfolio/index.html/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/sachinportfolio/index.html/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/sachinportfolio/index.html/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/sachinportfolio/index.html/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "a6addc3fb6605480614b992ede8cd434",
+    "url": "/sachinportfolio/index.html/static/media/googleAssistant.a6addc3f.svg"
+  },
+  {
+    "revision": "d3fccbe1db76b96f4f320d9c0f7da30c",
+    "url": "/sachinportfolio/index.html/static/media/ibm_logo.d3fccbe1.png"
+  },
+  {
+    "revision": "571a8584e69ecfbe326c8b8385fbbf54",
+    "url": "/sachinportfolio/index.html/static/media/iiitk_logo.571a8584.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/sachinportfolio/index.html/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "60ed0bd3f8b99ac1d29e74575f804aaa",
+    "url": "/sachinportfolio/index.html/static/media/iu_logo.60ed0bd3.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/sachinportfolio/index.html/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/sachinportfolio/index.html/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/sachinportfolio/index.html/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "069938a84edd6ebf383c318e9feb5d4c",
+    "url": "/sachinportfolio/index.html/static/media/muffito_logo.069938a8.png"
+  },
+  {
+    "revision": "1b16a123d982f8ea149615677bbc222a",
+    "url": "/sachinportfolio/index.html/static/media/nitk_logo.1b16a123.png"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/sachinportfolio/index.html/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "d73b11b6e18cfc4c2be9c3052671a071",
+    "url": "/sachinportfolio/index.html/static/media/optum_logo.d73b11b6.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/sachinportfolio/index.html/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/sachinportfolio/index.html/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/sachinportfolio/index.html/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/sachinportfolio/index.html/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "70b099d04e6ba455a46a1a365bdbe83b",
+    "url": "/sachinportfolio/index.html/static/media/sachin_rathod.70b099d0.png"
+  },
+  {
+    "revision": "2df33f0241e04a94b549897f1b21ff27",
+    "url": "/sachinportfolio/index.html/static/media/sap_logo.2df33f02.png"
+  },
+  {
+    "revision": "50be1773b0cf81e21d54c8009bca1737",
+    "url": "/sachinportfolio/index.html/static/media/sap_logo.50be1773.svg"
+  },
+  {
+    "revision": "2c497738be3831492aeb6bdc43f19154",
+    "url": "/sachinportfolio/index.html/static/media/stanford_logo.2c497738.png"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/sachinportfolio/index.html/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/sachinportfolio/index.html/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "0993e3246bf5146154391a0afef79b03",
+    "url": "/sachinportfolio/index.html/static/media/tiktok_logo.0993e324.png"
+  },
+  {
+    "revision": "d63630893eb8cb64af65f395d393c481",
+    "url": "/sachinportfolio/index.html/static/media/ui_ux_design.d6363089.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sachinportfolio/index.html/precache-manifest.a8a693cb7f46a0ad82947d1774ba1c4e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sachinportfolio/index.html/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
